3f8d8d63c2ff46008d04cba8d712ac1bb64893fc
Aline-So <maiaraaline581@hotmail.com>
2025-02-23 02:09:21 +0000

    JogoBATALHANAVALemandamento

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -9,6 +9,27 @@ int main() {
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+     
+    char linhas[10] = {'A', 'B','C','D','E','F','G','H','I','J'};
+    int colunas = {1, 2,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10};
+     int mat [10] [10];   
+    
+     printf("*** Bem Vindo ao Jogo 'BATALHA NAVAL' ***\n");
+      
+      printf("  ");
+      for(int i = 0; i < 10; i++){
+        printf("%c ", linhas[i]);
+    } 
+      
+      for(int j = 0; j > 10; j++){
+        printf("%d\n  ", colunas[j]);
+    }
+
+
+
+
+
+
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
