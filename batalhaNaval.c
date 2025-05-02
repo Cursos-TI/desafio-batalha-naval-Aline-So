d66f8c85adff1fd6f16148eb37408e480baa29dc
Aline-So <maiaraaline581@hotmail.com>
2025-05-02 17:13:06 +0000

    finalizadojogobatalhanaval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -10,71 +10,71 @@ int main() {
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
      
-    int linha[linhas];
-    char coluna[colunas];
-    int tabuleiro[linhas][colunas];
-    int navioHorizontal[NAVIO];
+    int linhas[10];
+    char colunas[10];
+    int tabuleiro[10][10];
+    int navioHorizontal[3];
     int navioVertical[NAVIO];
     int navioDiagonalPri[NAVIO];
     int navioDiagonalSec[NAVIO];
    
-    #define linhas 10
-    #define colunas 10
+    #define linha 10
+    #define coluna 10
     #define NAVIO 3
     #define MAR 0
     
      printf("*** Bem Vindo ao Jogo 'BATALHA NAVAL' ***\n");
     
-     for(int i = 0; i < linhas; i++) {
-        for (int j = 0; j < colunas; j++){
-          tabuleiro[i][j] = MAR;
+     for(int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++){
+          tabuleiro[i][j] = 0;
         }
      }
     
        int coordenadaX = 2;
        int coordenadaY = 3;
        
-       for (int i = 0; i < NAVIO; i++) {
-        navioHorizontal[i] = NAVIO;       
-        tabuleiro[coordenadaX][coordenadaY] = navioHorizontal[i];
-        coordenadaX++;
+       for (int i = 0; i < 3; i++) {
+        navioHorizontal[i] = 3;       
+        tabuleiro[2][3] = navioHorizontal[i];
+        2++;
       }
        
   coordenadaX = 5;
   coordenadaY = 7;
 
-  for (int j = 0; j < NAVIO; j++){
-   navioVertical[j] = NAVIO;
-   tabuleiro[coordenadaX][coordenadaY] = navioVertical[j];
-   coordenadaX++;
+  for (int j = 0; j < 3; j++){
+   navioVertical[j] = 3;
+   tabuleiro[5][7] = navioVertical[j];
+   5++;
 
   }
 
     int diagonalX = 1;
 
-    for (int i = 0; i < NAVIO; i++){
+    for (int i = 0; i < 3; i++){
 
-   navioDiagonalPri[i]= NAVIO;
-   tabuleiro[diagonalX][diagonalX] = navioDiagonalPri;
-   diagonalX++;   
+   navioDiagonalPri[i]= 3;
+   tabuleiro[1][1] = navioDiagonalPri;
+   1++;   
     }
 
     int diagonalSecX = 0;
     
-    for (int i = 0; i < NAVIO; i++){
-     navioDiagonalSec[i] = NAVIO;
-     tabuleiro[diagonalSecX] [9 - diagonalSecX] = navioDiagonalSec[i];
-     diagonalSecX++;
+    for (int i = 0; i < 3; i++){
+     navioDiagonalSec[i] = 3;
+     tabuleiro[0] [9 - 0] = navioDiagonalSec[i];
+     0++;
 
     }
     
     for (int i = 0; i < linhas; i++){
-      linhas[i] = + 1;
+      linha[i] = + 1;
     
     }
 
     for (int i = 0; < colunas; i++){
-      colunas[i] = 'A' + 1;
+      coluna[i] = 'A' + 1;
 
     }
     
@@ -83,20 +83,20 @@ int main() {
      printf("**TABULEIRO BATALHA - NAVAL**\n");
      printf("  ");
 
-     for(int i = 0; i < colunas; i++){
-      printf("%c ", colunas[i]);
+     for(int i = 0; i < 10; i++){
+      printf("%c ", coluna [i]);
 
      }
      
       printf("\n");
 
-      for ( int i = 0; i < linhas; i++){
-       printf("%2d", linhas[i]);
+      for ( int i = 0; i < 10; i++){
+       printf("%2d", linha[i]);
        for (int j =0; j < colunas; j++){
         printf("%2d", tabuleiro[i][j]);
        }
       printf("\n;")
-      }
+      
      printf("\n");
 
 
